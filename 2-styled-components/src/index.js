1b7df483ced768ad5f4e81d00334e9ee7d6f1616
indhu.pj <jayan.np@pg.com>
2022-05-02 04:14:36 +0530

    second task

--- 2-styled-components/src/index.js
+++ 2-styled-components/src/index.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import styled from "styled-components";
 
+const Header = styled.h1`
+  color: red;
+  font-size: 48px;
+  @media (max-width: 480px) {
+    color: green;
+    font-size: 24px;
+  }
+`;
+const Hero = styled(Header)`
+  color: blue;
+  font-size: 100px;
+`;
 const App = () => {
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <Hero>I am a Hero</Hero>
+      <Header>Hello React!</Header>
+    </div>
+  );
 };
 
 const rootElement = document.getElementById("root");
