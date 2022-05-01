4e5aec9e95d76a6c5396209b6d6965bc6b8f336d
indhu.pj <jayan.np@pg.com>
2022-05-01 13:44:35 +0530

    first task solution

--- 1-setup-react-project/webpack.config.js
+++ 1-setup-react-project/webpack.config.js
@@ -1,6 +1,27 @@
 var path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+  ],
   // TODO
 };
